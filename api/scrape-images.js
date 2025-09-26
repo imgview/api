e99b2550969f0307e0f98f4fea2430f20ea2cb19
imgview <komiklah007@gmail.com>
2025-09-26 12:16:21 +0700

    Update scrape-images.js

--- api/scrape-images.js
+++ api/scrape-images.js
@@ -39,17 +39,11 @@ export default async function handler(req, res) {
     // Fetch the webpage
     const response = await fetch(targetUrl.toString(), {
   headers: {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-    'Accept-Language': 'id-ID,id;q=0.9,en;q=0.8',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Connection': 'keep-alive',
-    'Upgrade-Insecure-Requests': '1',
-    'Sec-Fetch-Dest': 'document',
-    'Sec-Fetch-Mode': 'navigate',
-    'Sec-Fetch-Site': 'none',
-    'Cache-Control': 'max-age=0'
-  }
+  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+  'Accept-Language': 'id-ID,id;q=0.9,en;q=0.8',
+  'Referer': 'https://google.com/',
+  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+}
 });
 
     if (!response.ok) {
